c4521714a6f4990fd10ead9728877beb37de599d
Nima Heidari <nima.heidari.n@gmail.com>
2025-01-19 10:17:58 +0330

    AuthController Added

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+}
