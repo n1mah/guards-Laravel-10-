d0dc085abf13d2bedad4f2ce4d30e67627914f77
Nima Heidari <nima.heidari.n@gmail.com>
2025-01-19 10:20:59 +0330

    View Added (Login,Register)

---
+++ resources/views/admin/register.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>admin.register</title>
+</head>
+<body>
+<form action="{{ route('admin.register') }}" method="POST">
+    @csrf
+    <input type="text" name="name" placeholder="Name" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <input type="password" name="password_confirmation" placeholder="Confirm Password" required>
+    <button type="submit">Register</button>
+</form>
+
+</body>
+</html>
